a5bedf563963d5585fe220cd6ef8a4942e32fc49
CyanDark, Incorporated <admin@cyandark.com>
2016-11-24 19:00:01 -0600

    Code Quality Update
    Code Quality Update

--- PowerDNS.php
+++ PowerDNS.php
@@ -1,20 +1,14 @@
 <?php
-//  CyanDark Incorporated
-//  Copyright (c) 2012-2016 CyanDark, Inc. All Rights Reserved.
-//
-//  This software is furnished under a license and may be used and copied
-//  only  in  accordance  with  the  terms  of such  license and with the
-//  inclusion of the above copyright notice.  This software  or any other
-//  copies thereof may not be provided or otherwise made available to any
-//  other person.  No title to and  ownership of the  software is  hereby
-//  transferred.
-//
-//  You may not reverse  engineer, decompile, defeat  license  encryption
-//  mechanisms, or  disassemble this software product or software product
-//  license. CyanDark may terminate this license if you don't comply with
-//  any of the  terms  and conditions  set  forth in our end user license
-//  agreement (EULA).  In such event, licensee  agrees to return licensor
-//  or  destroy all copies  of  software  upon termination of the license
+/**
+ * PowerDNS API
+ *
+ * An HTTP API for PowerDNS.
+ *
+ * @package PowerDNS
+ * @copyright Copyright (c) 2012-2017 CyanDark, Inc. All Rights Reserved.
+ * @license http://www.wtfpl.net/about/ The Do What The Fuck You Want To Public License (WTFPL)
+ * @author CyanDark, Inc <support@cyandark.com>
+ */
 
 class PowerDNS{
 	protected $server;
@@ -26,30 +20,31 @@ class PowerDNS{
 	/**
 	 * Constructor
 	 *
-	 * @param string $UserName The PowerDNS MySQL username
-	 * @param string $Password The password of the database
-	 * @param string $DbName The PowerDNS database name
-	 * @param string $DbPort The port used for MySQL
+	 * @param string $server The PowerDNS Server
+	 * @param string $username The PowerDNS MySQL username
+	 * @param string $password The password of the database
+	 * @param string $dbname The PowerDNS database name
+	 * @param integer $dbport The port used for MySQL
 	 * @return stdClass The PowerDNS class to connect to DB
 	 */	
-	function __construct($Server, $Usermame, $Password, $Dbname='powerdns', $Dbport=3306){
-		$this->server = $Server;
-		$this->username = $Username;
-		$this->password = $Password;
-		$this->dbname = $Dbname;
-		$this->dbport = $Dbport;
+	function __construct($server, $username, $password, $dbname = 'powerdns', $dbport = 3306){
+		$this->server = $server;
+		$this->username = $username;
+		$this->password = $password;
+		$this->dbname = $dbname;
+		$this->dbport = $dbport;
 	}
 	
 	/**
-	 * Is a Valid Domain?
+	 * It's a Valid Domain?
 	 *
 	 * @param string $domain The domain for verify
 	 * @return boolean Result of the operation
 	 */	
 	function isValidDomain($domain){
-		return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain) //valid chars check
-				&& preg_match("/^.{1,253}$/", $domain) //overall length check
-				&& preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain)); //length of each label
+		return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain) // Valid chars check
+			&& preg_match("/^.{1,253}$/", $domain) // Overall length check
+			&& preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain)); // Length of each label
 	}
 	
 	/**
@@ -62,15 +57,14 @@ class PowerDNS{
 	function addDomain($domain, $solusvm_cid=null){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "INSERT INTO domains (name, type, solusvm_cid)
 				VALUES ('".htmlspecialchars($domain)."', 'NATIVE', '".intval($solusvm_cid)."')";
 		if ($conn->query($sql) === TRUE) {
 			return array("status" => "success", "msg" => "New record created successfully!");
-		} else {
-			return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		}
+		return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		$conn->close();
 	}
 
@@ -83,15 +77,14 @@ class PowerDNS{
 	function deleteDomain($domain_id){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "DELETE FROM domains WHERE id = ".intval($domain_id);
 		$sql2 = "DELETE FROM records WHERE domain_id = ".intval($domain_id);
 		if ($conn->query($sql) === TRUE && $conn->query($sql2) === TRUE) {
 			return array("status" => "success", "msg" => "Domain deleted successfully!");
-		} else {
-			return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		}
+		return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		$conn->close();
 	}
 
@@ -105,7 +98,7 @@ class PowerDNS{
 	function getDomainID($domain){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "SELECT * FROM domains WHERE name = '".htmlspecialchars($domain)."'";
 		$response = $conn->query($sql);
@@ -113,9 +106,8 @@ class PowerDNS{
 			while($row = $response->fetch_assoc()) {
 				return array("status" => "success", "msg" => "The domain ID is ".$row["id"], "id" => $row["id"], "solusvm_cid" => $row['solusvm_cid']);
 			}
-		} else {
-			return array("status" => "error", "msg" => "Error: Domain not found.");
 		}
+		return array("status" => "error", "msg" => "Error: Domain not found.");
 		$conn->close();
 	}
 	
@@ -131,15 +123,14 @@ class PowerDNS{
 	function addRecord($domain_id, $name, $type, $content, $ttl=1440, $prio=0){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "INSERT INTO records (domain_id, name, type, content, ttl, prio, change_date)
 				VALUES (".intval($domain_id).", '".htmlspecialchars($name)."', '".htmlspecialchars($type)."', '".htmlspecialchars($content)."', ".intval($ttl).", ".intval($prio).", ".time().")";
 		if ($conn->query($sql) === TRUE) {
 			return array("status" => "success", "msg" => "New record created successfully!");
-		} else {
-			return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		}
+		return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		$conn->close();
 	}
 
@@ -152,14 +143,13 @@ class PowerDNS{
 	function deleteRecord($record_id){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "DELETE FROM records WHERE id = ".intval($record_id);
 		if ($conn->query($sql) === TRUE) {
 			return array("status" => "success", "msg" => "Record deleted successfully!");
-		} else {
-			return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		}
+		return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		$conn->close();
 	}
 
@@ -172,7 +162,7 @@ class PowerDNS{
 	function getDomainsBySolusVMID($solusvm_cid){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "SELECT * FROM domains WHERE solusvm_cid = ".intval($solusvm_cid);
 		$domains = array();
@@ -182,9 +172,8 @@ class PowerDNS{
 				$domains[] = array("domain" => $row["name"], "id" => $row["id"], "solusvm_cid" => $row["solusvm_cid"]);
 			}
 			return array("status" => "success", "msg" => "The domains has been listed successfully", "domains" => $domains);
-		} else {
-			return array("status" => "error", "msg" => "Error: Domain not found.");
 		}
+		return array("status" => "error", "msg" => "Error: Domain not found.");
 		$conn->close();
 	}
 
@@ -197,7 +186,7 @@ class PowerDNS{
 	function getRecordsByDomainID($domain_id){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "SELECT * FROM records WHERE domain_id = ".intval($domain_id);
 		$records = array();
@@ -207,9 +196,8 @@ class PowerDNS{
 				$records[] = array("id" => $row["id"], "name" => $row["name"], "type" => $row["type"], "content" => $row["content"], "ttl" => $row["ttl"], "prio" => $row["prio"]);
 			}
 			return array("status" => "success", "msg" => "The rercord has been listed successfully", "zones" => $records);
-		} else {
-			return array("status" => "error", "msg" => "Error: Records not found.");
 		}
+		return array("status" => "error", "msg" => "Error: Records not found.");
 		$conn->close();
 	}
     
@@ -221,14 +209,13 @@ class PowerDNS{
 	function createSolusVMID(){
 		$conn = new mysqli($this->server, $this->username, $this->password, $this->dbname, $this->dbport);
 		if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
+    		throw new Exception("Connection failed: " . $conn->connect_error);
 		}
 		$sql = "ALTER TABLE domains ADD solusvm_cid VARCHAR(60)";
 		if ($conn->query($sql) === TRUE) {
 			return array("status" => "success", "msg" => "Column created successfully!");
-		} else {
-			return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		}
+		return array("status" => "error", "msg" => "Error: ".$sql." - ".$conn->error);
 		$conn->close();
 	}
 }
