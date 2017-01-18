1d6ef57fd4b89ef0de4417665b1af71d12e224f9
CyanDark, Inc <support@cyandark.com>
2017-01-18 10:33:27 -0600

    Update PowerDNS.php

--- src/PowerDNS.php
+++ src/PowerDNS.php
@@ -1,10 +1,7 @@
 <?php
 /**
- * PowerDNS API.
- *
  * An HTTP API for PowerDNS.
  *
- * @package PowerDNS
  * @copyright Copyright (c) 2012-2017 CyanDark, Inc. All Rights Reserved.
  * @license http://www.wtfpl.net/about/ The Do What The Fuck You Want To Public License (WTFPL)
  * @author CyanDark, Inc <support@cyandark.com>
